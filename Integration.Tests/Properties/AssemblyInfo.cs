8119f8e2fd792d4e00d4f21bd26cbe81f5113d46
BrunoJuchli <bruno.juchli@gmail.com>
2014-04-10 11:57:13 +0200

    adapted project skeleton

--- Integration.Tests/Properties/AssemblyInfo.cs
+++ Integration.Tests/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -14,7 +13,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © EROWA AG 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2bdaefd3-90bd-45b4-917a-73a813d12608")]
 
 // Version information for an assembly consists of the following four values:
 //
--- ninject.extensions.staticproxy.tests/Properties/AssemblyInfo.cs
+++ ninject.extensions.staticproxy.tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ninject.extensions.staticproxy.tests")]
+[assembly: AssemblyTitle("Ninject.Extensions.StaticProxy.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ninject.extensions.staticproxy.tests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("EROWA AG")]
+[assembly: AssemblyProduct("Ninject.Extensions.StaticProxy.Tests")]
+[assembly: AssemblyCopyright("Copyright © EROWA AG 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("10ef4509-11eb-4278-b113-b1eeebd238b6")]
 
 // Version information for an assembly consists of the following four values:
 //

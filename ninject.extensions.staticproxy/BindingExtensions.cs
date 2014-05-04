56641084b01f47fb4d19179c7371f4c7483a02ea
Bruno Juchli <bruno.juchli@innocreative.ch>
2014-05-04 17:26:59 +0200

    moved code to StaticProxy.Interceptor

--- ninject.extensions.staticproxy/BindingExtensions.cs
+++ ninject.extensions.staticproxy/BindingExtensions.cs
@@ -1,7 +1,8 @@
 ﻿namespace Ninject.Extensions.StaticProxy
 {
     using System;
-    using System.Globalization;
+
+    using global::StaticProxy.Interceptor.InterfaceProxy;
 
     using Ninject.Extensions.StaticProxy.SyntaxImplementation;
     using Ninject.Syntax;
@@ -23,47 +24,9 @@
         public static IBindingWhenInNamedWithOrOnSyntax<T> ToProxy<T>(this IBindingToSyntax<T> syntax, Action<IInterceptorBindingSyntax> configure)
             where T : class
         {
-            var interfaceType = typeof(T);
-            EnsureTypeIsInterface(interfaceType);
-
-            Type proxyType = RetrieveProxyType(interfaceType);
+            Type proxyType = InterfaceProxyHelpers.GetImplementationTypeOfInterface(typeof(T));
 
             return syntax.To(proxyType).Intercept(configure);
-        }
-
-        private static void EnsureTypeIsInterface(Type interfaceType)
-        {
-            if (!interfaceType.IsInterface)
-            {
-                string message = string.Format(
-                    CultureInfo.InvariantCulture,
-@"Only interfaces can be bound to a proxy with `.ToProxy(...)` and `{0}` is not an interface.
-If you want to intercept a class proxy, use `.Bind<IFoo>().To<Foo>().Intercept(...)` instead.",
-                    interfaceType.FullName);
-                throw new InvalidOperationException(message);
-            }
-        }
-
-        private static Type RetrieveProxyType(Type interfaceType)
-        {
-            string proxyTypeName = string.Concat(interfaceType.FullName, InterfaceImplementationSuffix);
-            Type proxyType = interfaceType.Assembly.GetType(proxyTypeName);
-            if (proxyType == null)
-            {
-                string message = string.Format(
-                    CultureInfo.InvariantCulture,
-@"There is no auto-generated implementation for interface `{0}`.
-Verify the following:
- - Put the attribute [StaticProxy] on the interface '{0}'. The attribute can be found in the nuget package `StaticProxy.Interceptor`.
- - Add the nuget packages `Fody` and `StaticProxy.Fody` to the assembly containing the interface `{0}`
-As a result there should be a class named `{1}` in the same assembly as the interface",
-                    interfaceType.FullName,
-                    proxyTypeName);
-
-                throw new InvalidOperationException(message);
-            }
-
-            return proxyType;
         }
     }
 }
